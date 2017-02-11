7b7710373388e71a4f5cc109b9a0408de88c4baf
Kayden <kayden@interana.com>
2017-02-11 11:20:36 -0800

    Add columns to coordinator schema

--- db/migrate/20170211185032_create_coordinators.rb
+++ db/migrate/20170211185032_create_coordinators.rb
@@ -1,6 +1,14 @@
 class CreateCoordinators < ActiveRecord::Migration[5.0]
   def change
     create_table :coordinators do |t|
+    	t.string   :first_name
+    	t.string   :last_name
+    	t.string   :email
+    	t.string   :phone
+    	t.string   :password_digest
+    	t.string   :category
+    	t.text		 :notes
+
 
       t.timestamps
     end
