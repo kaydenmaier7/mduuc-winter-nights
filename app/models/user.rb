51acff1e817fe79dd42359b43ed7ae36acbdeddc
Kayden <kayden@interana.com>
2017-02-26 01:40:12 -0800

    Add relationship between user and event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
+	has_many :events
 	validates :first_name, :last_name, :email,:phone, :password_digest, presence: true
 	validates :email, :phone, uniqueness: true
 end
