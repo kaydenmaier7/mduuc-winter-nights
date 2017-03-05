8352e9801384cfedf1cce5ea026acdda96bd890a
Kayden <kayden@interana.com>
2017-03-05 01:12:36 -0800

    Add date column to schema

--- db/migrate/20170226093531_create_events.rb
+++ db/migrate/20170226093531_create_events.rb
@@ -2,7 +2,8 @@ class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
     	t.string   :event_type, null: false
-    	t.text   	 :description, null: false	
+    	t.text   	 :description, null: false
+    	t.string 	 :date, null: false	
     	t.integer	 :max_volunteers, default: 10
 
       t.timestamps
