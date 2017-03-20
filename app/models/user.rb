99f78873bc35898f1e85bfb2bcb54c18d5ce8074
Kayden <kayden@interana.com>
2017-03-20 01:15:25 -0700

    Make all emails lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
 	
 	validates :first_name, :last_name, :email,:phone, :password_digest, presence: true
 	validates :email, :phone, uniqueness: true
+	before_save :downcase_email
+
+	def downcase_email
+		self.email.downcase!
+	end
 end
