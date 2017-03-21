bbda8eeda32998219434b5db1a9a1f4b6a3c78ca
Kayden <kayden@interana.com>
2017-03-20 19:01:39 -0700

    Sanitize inputs for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(email: session_params[:email])
+  	@user = User.find_by(email: session_params[:email].downcase)
   	if @user && @user.authenticate(session_params[:password])
   		login(@user)
   		redirect_to user_path(@user)
